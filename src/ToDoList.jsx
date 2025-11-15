bd3c4f6c5f7dc90f9ac260cdde5b65c109f046a0
mr.advert <mr.advert@yandex.ru>
2025-11-15 20:47:45 +0300

    ToDoList jsx added

---
+++ src/ToDoList.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react-refresh/only-export-components */
+import React, { useState } from "react";
+
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+
+  }
+
+  function addTask() {
+
+  }
+
+  function deleteTask() {
+
+  }
+
+  function moveTaskUp(index) {
+
+  }
+
+  function moveTaskDown(index) {
+
+  }
+
+  return (
+	<div className="to-do-list">
+
+	</div>
+  );
+}
+
+export default ToDoList();
